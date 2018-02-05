4d875c3215a9bf624c4c5752e58a955601d9492a
Sean Pringle <sean.pringle@gmail.com>
2018-02-05 12:42:16 +1100

    import jobqueue, workerpool

---
+++ workerpool/workerpool.go
@@ -0,0 +1,31 @@
+package workerpool
+
+type Pool struct {
+  Job  func(func())
+  Wait func()
+}
+
+func New(limit int) *Pool {
+  self := &Pool{}
+
+  sem := make(chan struct{}, limit)
+
+  self.Job = func(fn func()) {
+    sem <- struct{}{}
+    go func() {
+      fn()
+      <-sem
+    }()
+  }
+
+  self.Wait = func() {
+    for i := 0; i < limit; i++ {
+      sem <- struct{}{}
+    }
+    for i := 0; i < limit; i++ {
+      <-sem
+    }
+  }
+
+  return self
+}
